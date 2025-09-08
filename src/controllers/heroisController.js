1f231f6d295d899b7acded2e9d859fcefd358941
lucas <lucastavaresbento@gmail.com>
2025-09-08 09:04:18 -0300

    first commit

---
+++ src/controllers/heroisController.js
@@ -0,0 +1,63 @@
+import dados from "../models/dados.js"
+const { herois } = dados;
+
+
+// Dentro dos controllers é onde fica os verbos
+
+const getAllHerois = (req, res) => {
+    const { nome, universo, ativo } = req.query;
+    
+    let resultado = herois;
+
+    if (nome) {
+        resultado = resultado.filter((h) => 
+            h.nome.toLowerCase().includes(nome.toLowerCase())
+        );
+    }
+    
+    if (universo) {
+        resultado = resultado.filter((h) => 
+            h.universo.toLowerCase().includes(universo.toLowerCase())
+        );
+    }
+    
+    if (ativo !== undefined) {
+        resultado = resultado.filter((h) => h.ativo === (ativo === 'true'));
+    }
+
+    res.status(200).json({
+        total: resultado.length,
+        data: resultado
+    })
+}
+
+const createHeroi = (req, res) => {
+    const { nome, identidadeSecreta, poder, fraqueza, universo, ativo } = req.body;
+
+    // Validação de campos obrigatórios
+    if (!nome || !universo) {
+        return res.status(400).json({
+            success: false,
+            message: "Nome e universo são obrigatórios para um herói!",
+        })
+    }
+    // Criar novo herói
+    const novoHeroi = {
+        id: herois.length + 1,
+        nome,
+        identidadeSecreta,
+        poder,
+        fraqueza,
+        universo,
+        ativo,
+    };
+    herois.push(novoHeroi);
+
+    res.status(201).json({
+        success: true,
+        message: "Novo herói cadastrado!",
+        data: novoHeroi
+    })
+}
+
+export {getAllHerois, createHeroi}
